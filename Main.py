4eb1e452e5ef35583fadd194f1d7439fd8585888
Rubakdaniel <112389682+Rubakdaniel@users.noreply.github.com>
2022-10-10 14:47:50 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,9 +1,49 @@
 from typing import List
 
 def merge_sort(data) -> None:
-  # Write code here
-
-
+void merge(int arr[],int LeftInd,int RightInd,int MiddleInd)
+{
+  int i,j,k;
+  int n1=MiddleInd-LeftInd+1;
+  int n2=RightInd-MiddleInd;
+  int L[n1],R[n2];
+  for(i=0;i<n;i++)
+  {
+    L[i]=arr[LeftInd+i];
+  }
+  for(j=0;j<n;j++)
+  {
+    R[j]=arr[MiddleInd+1+j];
+  }
+  i=0,j=0,k=LeftInd;
+  while(i<n1 && j<n2)
+  {
+    if(L[i]<=R[j])
+    {
+      arr[k]=L[i];
+      i++;
+    }
+    else
+    {
+      arr[k]=R[j];
+      j++;
+    }
+    k++;
+  while(i<n1)
+  {
+    arr[k]=L[i];
+    i++;
+    k++;
+  }
+  while(j<n2)
+  {
+    arr[k]=R[j];
+    j++;
+    k++;
+  }
+  }
+}
+      
 # Do not change the following code
 input_data = input()
 data = []
